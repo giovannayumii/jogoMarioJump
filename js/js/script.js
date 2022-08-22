885f5e3613a418a73e91da468fccd7dfd0b5a5c5
giovannayumii <100042126+giovannayumii@users.noreply.github.com>
2022-08-22 16:01:51 -0300

    jogo jump

--- js/js/script.js
+++ js/js/script.js
@@ -1,7 +1,12 @@
 const mario = document.querySelector('.mario');
 const jump = () =>{
     mario.classList.add('jump');
+
+    setTimeout(()=>{
+        mario.classList.remove('jump');
+    },500);
 }
+
 
 
 /* Pular quando for pressionado qualquer tecla*/
