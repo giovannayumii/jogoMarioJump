ab1758ed12b46df0623830b99389951b7cd10414
giovannayumii <100042126+giovannayumii@users.noreply.github.com>
2022-08-22 16:13:26 -0300

    jogo

--- js/js/script.js
+++ js/js/script.js
@@ -1,11 +1,22 @@
 const mario = document.querySelector('.mario');
 const jump = () =>{
-    mario.classList.add('jump');
+    mario.classList.add('jump'); /*ele pula uma vez*/
 
     setTimeout(()=>{
-        mario.classList.remove('jump');
+        mario.classList.remove('jump');/*pula varias vezes*/
     },500);
 }
+
+const loop = setInterval(() =>{
+
+    const pipePosition = pipe.offsetLeft;
+
+    if (pipePosition <= 120){
+        pipe.style.animation = 'none';
+    }
+
+
+},10);
 
 
 
