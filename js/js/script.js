75038be4cf2bc9a4871bc2cbcddbc9716ee93abe
giovannayumii <100042126+giovannayumii@users.noreply.github.com>
2022-08-25 12:00:12 -0300

    commit

--- js/js/script.js
+++ js/js/script.js
@@ -1,4 +1,6 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
 const jump = () =>{
     mario.classList.add('jump'); /*ele pula uma vez*/
 
@@ -10,12 +12,22 @@ const jump = () =>{
 const loop = setInterval(() =>{
 
     const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if (pipePosition <= 120){
+    console.log(marioPosition);
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+
         pipe.style.animation = 'none';
+        pipe.syle.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.syle.bottom = `${marioPosition}px`;
+
+        mario.src = '../imagens/game-over.png';
+        mario.style.width ='75px'
+        mario.style.marginLeft = '50px'
     }
-
-
 },10);
 
 
